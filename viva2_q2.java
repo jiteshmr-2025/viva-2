b0f313feaf8d76b331c8d18cb39f2f3ba557d860
Xuzhe 123 <25006355@siswa.um.edu.my>
2025-12-12 18:50:52 +0800

    add question 2

---
+++ viva2_q2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class viva2_q2 {
+
+    public static boolean isBalanced(String str) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+            }
+
+            // If closing parenthesis appears before a matching opening one
+            if (count < 0) {
+                return false;
+            }
+        }
+
+        // Balanced only if count ends at 0
+        return count == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter expression: ");
+        String str = sc.nextLine();
+
+        if (isBalanced(str)) {
+            System.out.println("Balanced");
+        } else {
+            System.out.println("Not balanced");
+        }
+    }
+}
