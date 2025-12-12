94aa1b2130acb71a31fad118aa48dbb01bf1be3e
jitesh1059 <25006745@siswa.um.edu.my>
2025-12-12 12:52:16 +0800

    q4 updated with package

---
+++ Q4/src/q4/Q4.java
@@ -0,0 +1,182 @@
+package q4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ekitstrap
+ */
+public class Q4 {
+
+    /**
+     * 1. Read the three board rows from the user. 
+     * 2. Construct a 2D array of
+     * characters. 3. Call checkWinner() to determine the result. 4. Display the
+     * output as: a. "Winner: X" or "Winner: O" if a player has won, b. "No
+     * winner" if the game has no winning line.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Initialize Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Initialize board
+        char[][] board = new char[3][3];
+
+        for (int i = 0; i < board.length; i++) {
+            System.out.printf("Row %d: ", i + 1);
+            String temp = input.next();
+
+            // Make sure temp is 3
+            if (temp.length() != 3) {
+                System.out.println("Invalid number of inputs.");
+                return;
+            }
+
+            String validChars = "XO.";
+            for (char r : temp.toCharArray()) {
+                // If "XO." does not contain the character 'r'
+                if (validChars.indexOf(r) == -1) {
+                    System.out.println("Your board is invalid.");
+                    return;
+                }
+            }
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = temp.charAt(j);
+            }
+        }
+
+        int countX = countMoves(board, 'X');
+        int countO = countMoves(board, 'O');
+        // Checking if number of moves is valid
+        if (countX == countO || (countX - countO) == 1) {
+            // Call checkwinner()
+            char winner = checkWinner(board);
+            switch (winner) {
+                case 'X' ->
+                    System.out.println("Winner: 'X' ");
+                case 'O' ->
+                    System.out.println("Winner: 'O' ");
+                default ->
+                    System.out.println("No winner.");
+            }
+        } else {
+            System.out.println("Number of moves is not valid.");
+        }
+    }
+
+    /**
+     * Checks how many moves have been played.
+     *
+     * @param board The main playing board
+     * @param player The current character to be checked
+     * @return the number of moves made by a player
+     */
+    public static int countMoves(char[][] board, char player) {
+        int count = 0;
+        for (char[] board1 : board) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board1[j] == player) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     *
+     * @param board The main playing board
+     * @return The winner of the game or . if no winner
+     */
+    public static char checkWinner(char[][] board) {
+        // Check if the same symbol appears in any complete row, column, or one of the two diagonals.
+
+        // Check row and column
+        int countRowX = 0;
+        int countRowY = 0;
+        int countColX = 0;
+        int countColY = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                // Check rows
+                if (board[i][j] == 'X') {
+                    countRowX++;
+                    if (countRowX == 3) {
+                        return 'X';
+                    }
+                }
+                if (board[i][j] == 'O') {
+                    countRowY++;
+                    if (countRowY == 3) {
+                        return 'O';
+                    }
+                }
+
+                // Check columns
+                if (board[j][i] == 'X') {
+                    countColX++;
+                    if (countColX == 3) {
+                        return 'X';
+                    }
+                }
+
+                if (board[j][i] == 'O') {
+                    countColY++;
+                    if (countColY == 3) {
+                        return 'O';
+                    }
+                }
+            }
+            countRowX = 0;
+            countRowY = 0;
+            countColX = 0;
+            countColY = 0;
+        }
+
+        // Diagonal checking
+        /*
+        and also reverse of these
+        0, 0 -> i = j
+        1, 1 -> i = j
+        2, 2 -> i = j
+         */
+        // Everytime method starts we can reset these values
+        int countDiagForwardX = 0;
+        int countDiagForwardY = 0;
+        int countDiagBackX = 0;
+        int countDiagBackY = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = i; j <= i; j++) {
+                if (board[i][j] == 'X') {
+                    countDiagForwardX++;
+                    if (countDiagForwardX == 3) {
+                        return 'X';
+                    }
+                }
+                if (board[i][j] == 'O') {
+                    countDiagForwardY++;
+                    if (countDiagForwardY == 3) {
+                        return 'O';
+                    }
+                }
+                if (board[i][2 - j] == 'X') {
+                    countDiagBackX++;
+                    if (countDiagBackX == 3) {
+                        return 'X';
+                    }
+                }
+                if (board[i][2 - j] == 'O') {
+                    countDiagBackY++;
+                    if (countDiagBackY == 3) {
+                        return 'O';
+                    }
+                }
+            }
+        }
+
+        return '.';
+    }
+
+}
